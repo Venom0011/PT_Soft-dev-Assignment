d0973c82c3be26efc37a28e9a10d97659a7497b2
Venom0011 <abhishekmahajan255@gmail.com>
2023-01-05 14:12:02 +0530

    sql table added

---
+++ PT_Soft dev Assignment/demovalues.sql
@@ -0,0 +1,8 @@
+create table demo values(
+int id not null primary key auto_increment,
+Voltage varchar(250),
+Current varchar(250),
+Distance varchar(250),
+Duration varchar(250),
+Power varchar(250),
+);
